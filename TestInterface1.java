33f56ecf6f6e955ac149da80bfee7afd00bce111
Subhash-rvrjc <arvapallisubhash2003@gmail.com>
2024-07-16 15:10:15 +0530

    subhash

---
+++ TestInterface1.java
@@ -0,0 +1,21 @@
+interface Drawable{
+    void draw();
+}
+class Rectangle implements Drawable{
+     public void draw(){
+        System.out.println("drawing rectangle");
+     }
+    }
+     class Circle implements Drawable{  
+        public void draw(){
+            System.out.println("drawing circle");
+        }  
+        }  
+
+    public class TestInterface1{  
+        public static void main(String args[]){  
+        Drawable d=new Circle();
+        Drawable k=new Rectangle();
+        d.draw();
+        k.draw();
+        }}

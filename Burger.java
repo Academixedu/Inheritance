630ef99b8e49708608ca820bc31621958e6ed72a
dharmapuriyudhveer <dharmapuriyudhveer@gmail.com>
2024-07-16 20:00:56 +0530

    Interfaces and Inheritance

---
+++ Burger.java
@@ -0,0 +1,11 @@
+class Burger extends Food
+{
+    void eat()
+    {
+        System.out.println("I am eating a burger");
+    }
+    void Price()
+    {
+        System.out.println("The price of a burger is 100");
+    }
+}
--- InterFaceTest.java
+++ InterFaceTest.java
@@ -1,1 +1,12 @@
+public class InterFaceTest
+{
+    public static void main(String[] args) 
+    {
+        Chicken c=new Chicken1();
+        c.Types("Chicken Manchurian");
 
+        Chicken d=new Chicken2();
+        d.Types("Chicken 65");
+
+    }
+}

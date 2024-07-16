ba7e0d89739d77b4541581aa6faee365fe4ce2f2
Rishyendhar <rishyendhar.taduri@gmail.com>
2024-07-16 15:18:03 +0530

    Update ClassTest.java

--- ClassTest.java
+++ ClassTest.java
@@ -1,1 +1,17 @@
+public class ClassTest {
+    
+    public static void main(String[] args) {
+        Tiffins t=new Tiffins();
+        t.price();
+        t.quantity();
+
+        Tiffins d= new Dosa();
+        d.price();
+        d.quantity();
+
+        Tiffins b= new Bonda();
+        b.price();
+        b.quantity();
+    }
+}
 

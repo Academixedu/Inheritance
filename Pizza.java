3852ad27b0f8366130df80da0f3d84311592252c
Rishyendhar <rishyendhar.taduri@gmail.com>
2024-07-16 15:22:54 +0530

    Create Pizza.java

---
+++ Pizza.java
@@ -0,0 +1,14 @@
+public class Pizza implements Food {
+
+    @Override
+
+    public void price() {
+        System.out.println("Price of Pizza is 500");
+}
+
+@Override
+
+    public void quantity(){
+        System.out.println("Quantity of Pizza is 1");
+    }
+}

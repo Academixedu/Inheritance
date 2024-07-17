5ae57792c6772e92bfd7311c96aef15389279bd1
KOushikguptha <koushikguptha2003@gmail.com>
2024-07-17 22:57:09 +0530

    Inheritance

--- ClassTest.java
+++ ClassTest.java
@@ -1,1 +1,41 @@
+public class ClassTest {
+    public static void main(String[] args) {
+        Pancake p = new Pancake();
+        p.introduced();
+        p.rating();
+        
+        Waffles w = new Waffles();
+        w.introduced();
+        w.rating();
+        w.price();
+        
+        Dessert d = new Dessert();
+        d.introduced();
+        d.rating();
+        d.calories();
+        d.price();
+    }
+}
+
+class Pancake {
+    public void introduced() {
+        System.out.println("Dessert introduced in 600 BC");
+    }
+    
+    public void rating() {
+        System.out.println("4.5 rating");
+    }
+}
+
+class Waffles extends Pancake {
+    public void price() {
+        System.out.println("Price: 250");
+    }
+}
+
+class Dessert extends Waffles {
+    public void calories() {
+        System.out.println("93 calories");
+    }
+}
 
--- InterFaceTest.java
+++ InterFaceTest.java
@@ -1,1 +1,35 @@
+public class InterFaceTest {
+    public static void main(String[] args) {
+        Tacos t1 = new CheesyGorditaCrunch();
+        t1.introduced();
+        t1.rating();
+        t1.calories();
+        t1.price();
+    }
+}
+
+class CheesyGorditaCrunch implements Tacos {
+    public void introduced() {
+        System.out.println("Tacos introduced in 1905");
+    }
+    
+    public void rating() {
+        System.out.println("3.8 rating");
+    }
+    
+    public void calories() {
+        System.out.println("105 calories");
+    }
+    
+    public void price() {
+        System.out.println("Price: 400");
+    }
+}
+
+interface Tacos {
+    void introduced();
+    void rating();
+    void calories();
+    void price();
+}
 
